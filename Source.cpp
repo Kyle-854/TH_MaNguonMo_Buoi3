9719ee26efd65ee217348f48f9cd12a75758f4c6
Duongg <duowzuro@gmail.com>
2025-11-01 09:26:52 +0700

    ssCP

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,8 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+bool chinhphuong (int n);
+void ssCP(int a, int b);
 
 int tong(int a, int b){
 	return a + b
@@ -22,6 +24,28 @@ int hieu(int a, int b)
 
 }
 
+bool chinhphuong(int n)
+{
+	if (n < 0)
+		return false;
+	if can = sqrt(n);
+	return can*can == n;
+}
+
+void ssCP(int a, int b)
+{
+	bool A = laCP(a);
+	bool B = laCP(b);
+	if ( A && b)
+		cout<<"\nCa 2 la SNT";
+	else if (A)
+		cout<<"\nA la SNT";
+	else if (B)
+		cout<<"\nB la SNT";
+	else
+		cout<<"\na va b khong phai SNT";
+}
+
 int main()
 {
 	int a,b;
@@ -37,6 +61,7 @@ int main()
 	cout << "\nTich cua " << a << " va " << b << " la: " << tich(a, b) <<endl;
 
 	cout << "\nHieu cua " << a << " va " << b << " la: " << hieu(a, b) <<endl;
+	ssCP(a, b);
 
 	system("pause");
 	return 0;
