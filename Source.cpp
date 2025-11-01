76a1f273980a269782891746b89d961bdf4ef162
AnhKiet <anhkietngo0805@gmail.com>
2025-11-01 08:28:36 +0700

    them ham tong

--- Source.cpp
+++ Source.cpp
@@ -11,10 +11,14 @@ int hieu(int a, int b)
     return a - b;
 }
 
+int tong(int a, int b){
+	return a + b
+}
+
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	cout<<"\nTong cua 10 va 5: "<< tong(10, 5)<<endl;
 	cout << "\nHieu cua 10 va 5" << hieu(10, 5) << endl;
 	system("pause");
 	return 0;
