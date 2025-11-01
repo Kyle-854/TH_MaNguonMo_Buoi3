520c7619c15e61c7448d9da5fac28531ddff342e
AnhKiet <anhkietngo0805@gmail.com>
2025-11-01 09:08:55 +0700

    day ham sosanhavab len nhanh sosanhavab

--- Source.cpp
+++ Source.cpp
@@ -5,11 +5,11 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+void soSanhAB(int a, int b);
 
 int tong(int a, int b){
 	return a + b
 }
-
 
 int tich (int a, int b)
 {
@@ -20,6 +20,12 @@ int hieu(int a, int b)
 {
     return a - b;
 
+}
+
+void soSanhAB(int a, int b){
+	if (a > b) cout << a <<" lon hon "<<b<<endl;
+	else if (b > a) cout << b <<" lon hon "<< a <<endl;
+	else cout << a <<" bang "<<b<<endl;
 }
 
 int main()
@@ -37,6 +43,10 @@ int main()
 	cout << "\nTich cua " << a << " va " << b << " la: " << tich(a, b) <<endl;
 
 	cout << "\nHieu cua " << a << " va " << b << " la: " << hieu(a, b) <<endl;
+	
+	cout << "So sanh "<<a<<" va "<<b<<": "<< soSanhAB(a, b) <<end;
+	
+	
 
 	system("pause");
 	return 0;
